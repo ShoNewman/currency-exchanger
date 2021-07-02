be87a15bc941cf02992e050cf77d5a7390a1acb9
Shoshana Newman <sho.newman@gmail.com>
2021-07-02 14:05:03 -0700

    Add inputs to index and function to retrieve API call repsonse

--- src/main.js
+++ src/main.js
@@ -2,4 +2,16 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-import template from './template.js';
+import currencyService from './js/currency-service.js';
+
+$(document).ready(function() {
+  $('#weatherLocation').click(function() {
+    let location = $('#location').val();
+    currencyService.getCurreny(location)
+      .then(function(response) {
+        if (response) {
+          console.log(response.conversion_rates[location]);
+        } 
+      });
+  });
+});
