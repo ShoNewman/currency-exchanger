9bc4aac6687a60d945cdd3b6f91e6398b70deb1d
Shoshana Newman <sho.newman@gmail.com>
2021-07-02 14:10:24 -0700

    Refractored UI logic and added an async function

--- src/js/currency-service.js
+++ src/js/currency-service.js
@@ -1,11 +1,13 @@
 export default class currencyService {  
-  static async getCurreny(currency) { 
+  static async getCurreny() { 
     try { 
       const response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`);
       if (!response.ok) {
         throw Error(response.statusText);
       }
+      console.log(response.json);
       return response.json();
+      
     } catch(error) {
       return error.message;
     }
--- src/main.js
+++ src/main.js
@@ -4,14 +4,16 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import currencyService from './js/currency-service.js';
 
-$(document).ready(function() {
-  $('#weatherLocation').click(function() {
-    let location = $('#location').val();
-    currencyService.getCurreny(location)
-      .then(function(response) {
-        if (response) {
-          console.log(response.conversion_rates[location]);
-        } 
-      });
-  });
+async function makeApiCall(location) {
+  const response = await currencyService.getCurrency(location);
+  if (response) {
+    console.log(response.conversion_rates[location]);
+  }
+}
+
+
+$('#location').on('click', function() {
+  let location = $('#location').val();
+  makeApiCall(location);
 });
+
