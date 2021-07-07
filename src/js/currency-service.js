745a9fdedd1b450b56ca3feb44c0a2f71c51bc5e
Shoshana Newman <sho.newman@gmail.com>
2021-07-06 18:59:34 -0700

    Add API call for drop down list

--- src/js/currency-service.js
+++ src/js/currency-service.js
@@ -1,15 +1,29 @@
-export default class currencyService {  
-  static async getCurreny(location, amount) { 
-    try { 
-      const response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/USD/${location}/${amount}`);   
-      
+export default class currencyService {
+  static async getCurreny(location, amount) {
+    try {
+      const response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/USD/${location}/${amount}`);
+
+      if (!response.ok) {
+        throw Error(response.statusText);
+      }
+      return response.json();
+
+    } catch (error) {
+      return error.message;
+    }
+  }
+
+  static async getCodes() {
+    try {
+      const response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/codes`);
+
       if (!response.ok) {
         throw Error(response.statusText);
       }
       console.log(response.json);
       return response.json();
-      
-    } catch(error) {
+
+    } catch (error) {
       return error.message;
     }
   }
--- src/main.js
+++ src/main.js
@@ -6,24 +6,33 @@ import currencyService from './js/currency-service.js';
 
 async function makeApiCall(location, amount) {
   const response = await currencyService.getCurreny(location, amount);
+  let exchange = response.conversion_result;
   if (response) {
-    let exchange = response.conversion_result;
-    console.lot(response);
-    $('.showExchange').text(`${exchange}`);
+    $('.showExchange').text(`$${exchange}`);
   } else {
     $('.showErrors').text(`There was an error: ${response.message}`);
   }
 }
 
+async function makeApiCall2() {
+  const response = await currencyService.getCodes();
+  if (response) {
+    for (let i = 0; i < response.supported_codes.length; i++) {
+      let countries = response.supported_codes[i][1];
+      let code = response.supported_codes[i][0];
+      $('#location').append(`<option value="${code}">${countries}</option>`);
+    }
+  } else {
+    $('.showErrors').text(`There was an error: ${response.message}`);
+  }
+}
 
 $(document).ready(function () {
+  makeApiCall2();
   $('#submit').on('click', function (e) {
     e.preventDefault();
     let location = $('#location').val();
-    console.log(location);
     let currency = parseInt($('#exchange').val());
-    console.log(currency);
     makeApiCall(location, currency);
-
   });
 });
